c95c9455d6bbacc24e69b11b03945a781e1f8f17
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-27 17:53:46 -0400

    Minor update

---
+++ include/packet.h
@@ -0,0 +1,28 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <stdint.h>
+
+# define ETH_HDR_LEN 14
+# define ARP_PKT_LEN 28
+# define MACADDR_LEN 6
+# define IP_LENGTH 4
+
+typedef struct{
+    uint8_t targetMac[MACADDR_LEN];
+    uint8_t senderMac[MACADDR_LEN];
+    uint16_t etherType;
+}eth_header_t;
+
+typedef struct{
+    uint16_t hardwareType;
+    uint16_t protocolType;
+    uint8_t hardwareLen;
+    uint16_t opcode;
+    uint8_t sender_mac[MACADDR_LEN];
+    uint8_t sender_ip[IP_LENGTH];
+    uint8_t target_mac[MACADDR_LEN];
+    uint8_t target_ip[IP_LENGTH];
+}arp_packet_t;
+
+#endif // PACKET_H
