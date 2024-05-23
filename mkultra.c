891e53014b31848501576515139eb40a82864c56
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-22 20:09:06 -0400

    Update

--- mkultra.c
+++ mkultra.c
@@ -1,13 +1,4 @@
-/*
-* Created by: Asad Zia
-* 
-* Description:
-* A packet sniffer created using the pcap library. 
-* The default setting allows one to read live data from ALL interfaces. The -i option is used to read from a particular interface.
-* The -f option is used to read recorded data from a file. The interval for displaying the data can be set by using the -d option.
-* The -N option can be used to adjust the number of talkers we want to observe exchanging the packets.
-*
-*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
